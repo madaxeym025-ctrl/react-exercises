3218c54ea1188a11d5d651eda693c03238087dad
madaxeym025-ctrl <madaxeym025@gmail.com>
2026-07-02 08:30:50 +0300

    exercise3

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import UseCard from "./Exercise1/useCard"
+
+function App(){
+  return<>
+  <UseCard />
+  <UseCard />
+  <UseCard />
+  </>
+}
+export default App
---
+++ src/Exercise1/UseCard.jsx
@@ -0,0 +1,10 @@
+const UseCard = ()=>{
+    let name = "maxamed"
+    let gmail = "madaxey.@mail"
+
+    return<>
+    <h1>your name is {name} </h1>
+    <h1> this is the {gmail} </h1>
+    </>
+}
+export default UseCard
